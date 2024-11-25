921cc6fd2494f5e3e9b916b5ff388092f5b46aea
Georgiy Sgibnev <georgiy@lab50.net>
2024-11-25 12:14:48 +0300

    Лучше чистим Debian

--- packer_templates/debian/scripts/cleanup.sh
+++ packer_templates/debian/scripts/cleanup.sh
@@ -11,6 +11,14 @@ dpkg --list | awk '{ print $2 }' | grep 'linux-image-[1-9].*' | grep -v `uname -
 dpkg --list | awk '{ print $2 }' | grep 'kmouth\|bluedevil\|kamera\|smartmontools' | xargs sudo apt-get -y purge;
 sudo apt-get -y purge gcc build-essential git
 dpkg --list | awk '{ print $2 }' | grep -- '-dev\(:[a-z0-9]\+\)\?$' | xargs sudo apt-get -y purge;
+
+sudo apt-get purge -y nftables
+sudo apt-get purge -y libsqlite3-0
+sudo apt-get purge -y mc unzip
+sudo apt-get purge -y ispell ibritish iamerican
+sudo apt-get purge -y xauth libx11-data libx11-6
+sudo apt-get purge -y firmware-linux-free laptop-detect
+sudo apt-get purge -y libjansson4
 sudo apt-get -y autoremove
 
 # Clean an apt cache.
