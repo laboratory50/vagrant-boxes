080516c7748b00525fd0072d898cc605229c763b
Georgiy Sgibnev <georgiy@lab50.net>
2025-06-26 16:55:40 +0300

    Починка сборки salvia-kde

--- packer_templates/basealt/scripts/install-kde.sh
+++ packer_templates/basealt/scripts/install-kde.sh
@@ -10,5 +10,9 @@ fi
 
 sudo apt-get update
 sudo apt-get install -y kde5-mini lightdm-kde-greeter lightdm
-sudo apt-get install -y kernel-modules-drm-std-def kernel-modules-staging-std-def xorg-drv-video xorg-drv-spiceqxl
+if echo "${PACKER_BUILD_NAME}" | grep --quiet 'aronia'
+then
+    sudo apt-get install -y kernel-modules-drm-std-def kernel-modules-staging-std-def
+fi
+sudo apt-get install -y xorg-drv-video xorg-drv-spiceqxl
 sudo systemctl set-default graphical.target
